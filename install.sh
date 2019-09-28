eac8001ccbc4279865c7ae92515cd2792743d160
Adrian Dombeck <adrian.dombeck@rub.de>
2019-09-28 14:18:47 +0200

    Remove install-oh-my-zsh.sh
    I cherry-picked the files I need into dotfiles/.zsh, so I don't have to
    install oh-my-zsh separately anymore.

--- install.sh
+++ install.sh
@@ -3,14 +3,6 @@ set -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-
-# Install oh-my-zsh
-if [ -d $HOME/.oh-my-zsh ]; then
-	echo "oh-my-zsh already installed. skipping."
-else
-	$DIR/install-oh-my-zsh.sh
-fi
-
 # Install powerline-fonts for the agnoster theme
 sudo apt install -y fonts-powerline
 
