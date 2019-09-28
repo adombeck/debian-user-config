9ce770cbccc1c0ce4d629c1ebb99ba718ebfa851
Adrian Dombeck <adrian.dombeck@rub.de>
2019-09-28 12:57:37 +0200

    packages: Fix install-sources.sh

--- packages/install-sources.sh
+++ packages/install-sources.sh
@@ -1,11 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 
-read -p "WARNING: This will install Debian unstable sources and upgrade packages to Debian unstable. Do you want to continue? (y/n)"
+cat << EOF
+WARNING: This script will:
+ * install Debian unstable sources
+ * upgrade all packages to Debian unstable
+Do you want to continue? (y/N)
+EOF
+
+read
+if [ "$REPLY" != "y" ]; then
+	exit 1
+fi
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 sudo apt install apt-transport-tor
 
 if [ -f /etc/apt/sources.list ]; then
-	sudo mv /etc/apt/sources.list /etc/apt/sources.list.orig
+	sudo mv -i /etc/apt/sources.list /etc/apt/sources.list.orig
 fi
 
+cp "$DIR/sources.list" /etc/apt/sources.list
+
 sudo apt-get update && sudo apt-get dist-upgrade -y
+
