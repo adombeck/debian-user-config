1bcfb0a381bf2709bc8cb5d4267eee14b3bd144f
Adrian Dombeck <adrian.dombeck@rub.de>
2019-10-16 18:45:51 +0200

    dotfiles: Better support for .d directories
    Instead of symlinking the whole .d directory, symlink the config files
    in the directory.

--- dotfiles/install.sh
+++ dotfiles/install.sh
@@ -5,35 +5,54 @@ set -u
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for f in "$DIR"/.[!.]*; do
-	if [[ "$f" == "$DIR/.gitmodules" ]] || [[ "$f" == "$DIR/.nfs"* ]]; then
-		echo "Skipping $f"
-		continue
+maybe_create_symlink() {
+  local source="${1}"
+  local target="$HOME/$(realpath --relative-to="$DIR" "${source}")"
+  echo "target: $target"
+
+  if [[ "$f" == "$DIR/.gitmodules" ]] || [[ "${source}" == "$DIR/.nfs"* ]]; then
+		echo "Skipping ${source}"
+		return
 	fi
 
-    # Check if the dotfile is already present in the home directory
-    target="$HOME/$(basename "$f")"
-    if [ -L "${target}" ]; then
-        # The file is a symlink, so it's probably safe to just delete it.
-		echo "Updating symlink for $f"
-        rm "${target}"
-	    ln -s "$f" "$HOME"
-		continue
-    fi
+	# If the target is a .d directory, create symlinks for the config
+	# files in the directory, instead of for the directory itself.
+	# This allows the user to still put their own config files in there
+	# without committing them to to git.
+	if [ -d "${source}" ] && [ "${source: -2}" = ".d" ]; then
+	  mkdir -p "${target}"
+	  for f in "${source}"/*; do
+	    maybe_create_symlink "$f"
+	  done
+	  return
+	fi
 
-    if [ -e "${target}" ]; then
-		echo -e "The file ${target} already exists.\nDo you want to move the file to ${target}.orig? Else it will be skipped. (y/N) "
-		read
-		if [ "$REPLY" != "y" ]; then
-		    echo "Skipping $f"
-            continue
-        else
-            mv -i "${target}" "${target}.orig"
-        fi
-    fi
+  # Check if the dotfile is already present in the home directory
+  if [ -L "${target}" ]; then
+      # The file is a symlink, so it's probably safe to just delete it.
+  echo "Updating symlink for ${source}"
+      rm "${target}"
+    ln -s "${source}" "${target}"
+  return
+  fi
 
-	echo "Creating symlink for $f"
-	ln -s "$f" "$HOME"
+  if [ -e "${target}" ]; then
+  echo -e "The file ${target} already exists.\nDo you want to move the file to ${target}.orig? Else it will be skipped. (y/N) "
+  read
+  if [ "$REPLY" != "y" ]; then
+      echo "Skipping ${source}"
+          return
+      else
+          mv -i "${target}" "${target}.orig"
+      fi
+  fi
+
+	echo "Creating symlink for ${source}"
+	ln -s "${source}" "${target}"
+}
+
+for f in "$DIR"/.[!.]*; do
+	maybe_create_symlink "$f"
 done
 
 read -p "Do you want to set zsh as your default shell? (Y/n)"
