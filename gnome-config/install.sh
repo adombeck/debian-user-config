cca415e0e0527a9f8fff7cb02cc5b344bac05b2d
Adrian Dombeck <adrian.dombeck@rub.de>
2022-11-27 23:28:54 +0100

    gnome-config: Install the gnome-clipboard-history extension

--- gnome-config/install.sh
+++ gnome-config/install.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
+set -eu
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Install gnome-shell extensions
+make -C "${DIR}/extensions/gnome-clipboard-history" bundle
+gnome-extensions install --force "${DIR}/extensions/gnome-clipboard-history/bundle.zip"
+
+# Load the GNOME config
 dconf load /org/gnome/ < "$DIR/gnome.dconf"
 
 # Enable udisks TCRYPT support
