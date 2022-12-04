aec989bd66a60664121577e5ecd622189e2db941
Adrian Dombeck <adrian.dombeck@rub.de>
2022-12-04 23:23:26 +0100

    flatpak: Remove com.spotify.Client
    The Spotify app doesn't work natively on Wayland yet. My workaround is
    to use the PWA in chromium.

--- flatpak/install-flatpaks.sh
+++ flatpak/install-flatpaks.sh
@@ -7,7 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 
 flatpak install --system flathub com.jetbrains.PyCharm-Community
-flatpak install --system flathub com.spotify.Client
 flatpak install --system flathub org.chromium.Chromium
 
 "${DIR}/apply-overrides.sh"
