0f3e043a39a395c12f7c8dc6fbbf418b8e3e3354
Adrian Dombeck <adrian.dombeck@gmail.com>
2025-05-27 10:47:35 +0200

    Enable systemd units via dotfiles

--- install.sh
+++ install.sh
@@ -7,9 +7,6 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Install dotfiles
 "$DIR/dotfiles/install.sh"
 
-# Install systemd services
-"$DIR/systemd/install.sh"
-
 # Configure GNOME
 "$DIR/gnome-config/install.sh"
 
