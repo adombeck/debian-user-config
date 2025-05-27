40cdf8b712a54614031966e412be7454c2580a12
Adrian Dombeck <adrian.dombeck@gmail.com>
2025-05-27 10:48:44 +0200

    Fix guake/install.sh

--- guake/install.sh
+++ guake/install.sh
@@ -7,7 +7,7 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 sudo apt-get install -y guake
 
 echo "Loading guake config"
-dconf load /apps/guake/ < "$DIR/guake.dconf"
+dconf load /org/guake/ < "$DIR/guake.dconf"
 
 echo "Adding keybinding to toggle Guake via F10"
 set-gnome-keybinding.py "Toggle Guake" "guake --toggle" "F10"
